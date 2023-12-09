1246a5e3a771a5356af9c4b3a466dd7df42bf89e
kiwi2kiwi <yan!git13>
2023-12-09 13:03:32 +0100

    pre backprop inclusion into simulation

---
+++ code/Backprop.py
@@ -0,0 +1,105 @@
+from Neuron import *
+import numpy as np
+
+from sklearn import datasets
+iris = datasets.load_iris()
+from sklearn.utils import shuffle
+
+X = np.array(iris.data)
+y = np.array(iris.target)
+X, y = shuffle(X, y)
+X_train = X[:100]
+X_test = X[100:]
+y_train = np.array([y[:100]])
+y_test = np.array([y[100:]])
+
+train_data = np.concatenate((X_train, y_train.T), axis=1)
+test_data = np.concatenate((X_test, y_test.T), axis=1)
+
+input_layer = [Input_Neuron(0), Input_Neuron(1), Input_Neuron(2), Input_Neuron(3)]
+neuron_number = 4
+
+weights = []
+
+layer_layout = [[1]]
+
+layers = []
+active_neurons = []
+for idx, l in enumerate(layer_layout):
+    layer = []
+    for neur in l:
+        n = Neuron(neuron_number)
+        neuron_number += 1
+
+        if idx == 0:
+            for i in input_layer:
+                n.parent_connections[i] = [i, 100, []]
+        else:
+            for i in layers[idx-1]:
+                n.parent_connections[i] = [i, 100, []]
+        layer.append(n)
+        active_neurons.append(n)
+    layers.append(layer)
+
+for layer in layers:
+    for n in layer:
+        n.wire()
+
+
+
+def error_function(pre,tar):
+    return round((pre - tar)**2,3)
+
+def deriv_error_function(pre,tar):
+    return 2*(pre - tar)
+
+def compute_error(target):
+    for idx, n in enumerate(layers[-1]):
+        pred = n.activation()
+        print("error: ", error_function(pred, target[idx]))
+
+def predict(slice_of_data):
+    for idx, inp in enumerate(input_layer):
+        inp.set_input(slice_of_data[idx])
+    prediction = []
+    for i in layers[-1]:
+        prediction.append(i.activation())
+    return prediction
+
+
+def backprop(target):
+    compute_error(target)
+    learning_rate = 0.005
+    for idx, n in enumerate(layers[-1]):
+        error_through_a_zero = deriv_error_function(n.activation(), target[idx])
+        n.gradient_descent(error_through_a_zero, learning_rate)
+
+
+def train(data):
+
+    for ds in data:
+        predict(ds[:-1])
+        backprop([ds[-1]])
+
+
+train(train_data)
+
+def do_test(data):
+    pred = []
+    for ds in data:
+        pred.append(round(predict(ds[:-1])[0],0))
+        for a in active_neurons:
+            a.reset_neuron()
+    target = data[:,-1]
+
+    from sklearn.metrics import accuracy_score
+    print("accuraccy: ", accuracy_score(target, pred))
+    visual = np.concatenate((np.array([pred]), [target]), axis=0)
+    print("done")
+
+
+
+do_test(test_data)
+
+
+print("end")
---
+++ code/Neuron.py
@@ -0,0 +1,147 @@
+import numpy as np
+import random
+
+
+class Neuron():
+    def __init__(self, coordinate, base_space, output_neuron = False, name = "not_set"):
+        super(Neuron, self).__init__()
+#        self.name = name
+        self.parent_connections = {} # closer to input
+        self.children_connections = {} # closer to output
+        self.output = 0
+        self.activated = False
+        self.output_neuron = output_neuron
+
+        self.coordinate = coordinate
+        self.name = ",".join([str(self.coordinate.x), str(self.coordinate.y), str(self.coordinate.z)])
+        self.hash_val = int(''.join(c for c in self.name if c.isdigit()))
+        self.base_space = base_space
+        print("Hey, im a neuron!")
+
+    def reset_neuron(self):
+        self.output = 0
+        self.activated = False
+        for p in self.parent_connections.keys():
+            self.parent_connections[p][2] = []
+        for c in self.children_connections.keys():
+            self.children_connections[c][2] = []
+
+
+    def wire(self):
+
+        #weight = 0.8
+        for p in self.parent_connections.keys():
+            weight = round(random.uniform(0, 1), 2)
+            parent_connection = self.parent_connections[p]
+            parent = parent_connection[0]
+            parent.children_connections[self.__hash__()] = [self, weight, []]
+            self.parent_connections[p] = [parent, weight, []]
+
+    def gradient_normalisation(self, gradient):
+        #return gradient
+        return max(min(0.5,gradient),-0.5)
+        #return ((1. / (1 + np.exp(-gradient)))-0.5) * 1
+
+    def change_weight(self):
+        for p in self.parent_connections.keys():
+            parent_connection = self.parent_connections[p]
+            parent = parent_connection[0]
+            new_weight_to_parent = parent_connection[1] - sum(parent_connection[2])
+            parent.children_connections[self.__hash__()] = [self, new_weight_to_parent, [new_weight_to_parent]]
+            self.parent_connections[p] = [parent, new_weight_to_parent, [new_weight_to_parent]]
+
+
+    def get_weight(self, parent):
+        return self.parent_connections[parent][1]
+
+    def gradient_descent(self, bis_hier, learning_rate):
+
+        ab_hier = self.a_null_a_eins() * bis_hier
+        for p in self.parent_connections.keys():
+            parent_connection = self.parent_connections[p]
+            self.parent_connections[p][0].gradient_descent(ab_hier, learning_rate)
+
+            error_durch_w = self.a_null_w_parent(parent_connection[0]) * bis_hier
+
+            self.parent_connections[p][2].append(self.gradient_normalisation((learning_rate * error_durch_w)))
+
+            print("weight: ", round(parent_connection[1],2), " adjust: ", round(sum(parent_connection[2]),2))
+        self.change_weight()
+        self.reset_neuron()
+
+
+    def activation_function(self, z):
+        return z
+
+    #        return 1. / (1 + np.exp(-z))
+
+    def deri_activation_function(self, z):
+        return z
+
+    def activation(self):
+        if self.activated:
+            return self.output
+
+        summation = 0
+        for p in self.parent_connections.keys():
+            parent_connection = self.parent_connections[p]
+            summation += parent_connection[0].activation() * parent_connection[1]
+        self.output = self.activation_function(summation)
+        self.activated = True
+        return self.output
+
+    def deri_activation(self):
+        summation = 0
+        for p in self.parent_connections.keys():
+            summation += self.parent_connections[p].deri_activation()
+        return self.deri_activation_function(summation)
+
+    def a_null_w_parent(self, parent):
+        return self.deri_activation_function(parent.activation())
+
+    def a_null_a_eins(self):
+        summation = 0
+        for p in self.parent_connections.keys():
+            parent_connection = self.parent_connections[p]
+            summation += parent_connection[1]
+        return self.deri_activation_function(summation)
+
+    def __hash__(self):
+        return self.hash_val
+
+
+class Input_Neuron():
+    def __init__(self, coordinate, base_space, name = "not set"):
+        super(Input_Neuron, self).__init__()
+        self.children_connections = {} # closer to output
+        self.output = 0
+        self.activated = False
+#        self.name = name
+
+        self.coordinate = coordinate
+        self.name = ",".join([str(self.coordinate.x), str(self.coordinate.y), str(self.coordinate.z)])
+        self.hash_val = int(''.join(c for c in self.name if c.isdigit()))
+        self.base_space = base_space
+
+    def gradient_descent(self, a, b):
+        pass
+
+    def set_input(self, input):
+        self.activated = True
+        self.output = input
+
+    def activation_function(z):
+        return 1. / (1 + np.exp(-z))
+
+    def activation(self):
+        return self.output
+
+    def deri_activation(self):
+        return self.output
+
+    def reset_neuron(self):
+        self.output = 0
+        self.activated = False
+
+    def __hash__(self):
+        return self.hash_val
