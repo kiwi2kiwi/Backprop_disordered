Network fixed root of issue was somewhere in the combined error propagation from all the classes. I changed the network to do backpropagation for each class separately

code/Neural_network/Backprop.py
@@ -1,3 +1,4 @@
+import numpy
 from Neural_network.Neuron import *
 import numpy as np
 import sklearn.metrics
@@ -15,10 +16,10 @@ class Backpropagation:
 
 
     def error_function(self, pre,tar):
-        return ((tar - pre)**2)
+        return ((tar - pre) **2)
 
     def deriv_error_function(self, pre,tar):
-        return (2*(pre - tar))
+        return (2 * (pre - tar))
 
 
     def compute_error(self, target):
@@ -41,7 +42,7 @@ class Backpropagation:
         return prediction
 
 
-    def backprop(self, target, learning_rate):
+    def backprop(self, target, learning_rate, slice_of_data):
 
         for idx, n in enumerate(self.base_space.output_neuron_dict.values()):
             if self.base_space.verbal:
@@ -51,16 +52,19 @@ class Backpropagation:
             #            target_dict = dict(zip(unique, counts))
             #            class_balancer = sum(target_dict.values()) / target_dict[target[idx]]
             #            class_balancer = 1 / (1 + np.exp(-class_balancer))
-
+            self.reset_neurons()
+            self.reset_neuron_gradients()
+            for input_idx, input_neuron in enumerate(self.base_space.input_neuron_dict.values()):
+                input_neuron.set_input(slice_of_data[input_idx])
             n_out = n.activation()
             y_true = target[idx]
             error_through_net_out = self.deriv_error_function(n_out, y_true)
             n.error_for_output_neuron = error_through_net_out
-
-        for idx, n in enumerate(self.base_space.output_neuron_dict.values()):
             n.gradient_descent(learning_rate, depth_counter=1)# * class_balancer)
 
-        self.reset_neuron_gradients()
+
+        # for idx, n in enumerate(self.base_space.output_neuron_dict.values()):
+
 
         #for idx, n in enumerate(self.base_space.output_set):
         #n.gradient_descent(learning_rate)
@@ -71,7 +75,7 @@ class Backpropagation:
         for idx, ds in enumerate(x):
             # print("sample: ",idx)
             self.predict(ds)
-            self.backprop(y[idx], learning_rate)
+            self.backprop(y[idx], learning_rate, ds)
             if loss_array is None:
                 loss_array = self.compute_error(y[idx])
             else:
@@ -141,19 +145,36 @@ class Backpropagation:
     def iris_evaluation(self, x, y, metric = "acc"):
         pred = []
         for ds in x:
-            pred.append([int(round(i*1,0)) for i in self.predict(ds)])
+            try:
+                prediction = self.predict(ds)
+
+                pred_temp = numpy.zeros_like(prediction)
+                pred_temp[numpy.argmax(prediction)] = 1
+                pred.append(pred_temp)
+                # pred.append([int(round(i,0)) for i in self.predict(ds)])
+            except:
+                print("stop")
+                [int(round(i * 1, 0)) for i in self.predict(ds)]
+                [int(round(i * 1, 0)) for i in self.predict(ds)]
+                [int(round(i * 1, 0)) for i in self.predict(ds)]
+                [int(round(i * 1, 0)) for i in self.predict(ds)]
             self.reset_neurons()
 
-        target = y*1
-        if metric == "acc":
-            acc = sklearn.metrics.accuracy_score(target, pred)
-            return acc
-        if metric == "recall":
-            recall = sklearn.metrics.recall_score(target, pred, zero_division=1, average=None)
-            return recall
-        if metric == "precision":
-            precision = sklearn.metrics.precision_score(target, pred, zero_division=1, average=None)
-            return precision
-        if metric == "f1":
-            f1 = sklearn.metrics.f1_score(target, pred, zero_division=1, average=None)
-            return f1
+        target = y
+        # target = np.argmax(target, axis=1)
+        # pred = np.argmax(pred, axis=1)
+        try:
+            if metric == "acc":
+                acc = sklearn.metrics.accuracy_score(target, pred)
+                return acc
+            if metric == "recall":
+                recall = sklearn.metrics.recall_score(target, pred, zero_division=1, average=None)
+                return recall
+            if metric == "precision":
+                precision = sklearn.metrics.precision_score(target, pred, zero_division=1, average=None)
+                return precision
+            if metric == "f1":
+                f1 = sklearn.metrics.f1_score(target, pred, zero_division=1, average=None)
+                return f1
+        except:
+            return 0

code/Neural_network/Neuron.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import Neural_network.Axon
 import time
+import math
 
 
 class Neuron():
@@ -86,7 +87,9 @@ class Neuron():
                 # if not self.base_space.fast:
                 #     #                print("from ", self.name, " to ", parent_connection.parent.name)
                 #     print("weight: ", round(parent_connection.get_weight(), 3), " adjust by: ", round(-gradient, 4))
-                new_weight = parent_connection.get_weight() - gradient
+                new_weight = max(min(10,parent_connection.get_weight() - gradient),-10)
+                if math.isnan(new_weight):
+                    print("pause")
 
                 # print("Axon: ", parent_connection.name, " weight: ", round(parent_connection.get_weight(), 3), " adjust by: ", round(-gradient, 4))
                 parent_connection.weight = new_weight
@@ -150,7 +153,9 @@ class Neuron():
             # Appending the gradient
             if gradient != 0:
                 if self.base_space.verbal:
-                    print("Gradient descent to neuron", self.name, " gradient: ", gradient)
+                    print("Gradient descent to neuron: ", self.name, " gradient: ", gradient)
+                if math.isnan(learning_rate*gradient*depth_counter):
+                    print("pause")
                 self.parent_connections[p].new_weights.append(learning_rate * gradient * depth_counter)
             #   self.parent_connections[p].new_weights.append(self.gradient_normalisation(learning_rate * gradient))
             if not parent_connection.parent.started:
@@ -170,12 +175,16 @@ class Neuron():
 
 
     def activation_function(self, z):
-        return (1. / (1 + np.exp(-z)))
-        return z
+        if math.isnan(z):
+            print("pause")
+        return z # linear
+        return (1. / (1 + np.exp(-z))) # sigmoid
 
     def deri_activation_function(self):
-        return self.activation() * (1 - self.activation())
-        return self.activation()
+        if math.isnan(self.activation()):
+            print("pause")
+        return 1 # linear
+        return self.activation() * (1 - self.activation()) # sigmoid
 
     def activation(self):
         if self.activated:
@@ -251,6 +260,8 @@ class Input_Neuron():
         pass
 
     def set_input(self, input):
+        if math.isnan(input):
+            print("pause")
         self.input = input
 
     def activation_function(self, z):

code/Neural_network/nn_execution.py
@@ -1,6 +1,7 @@
 import Neural_network.Neuron_space
 import Neural_network.Backprop
 import numpy as np
+import math
 from sklearn import datasets
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
@@ -34,15 +35,15 @@ def running_the_network(n):
     X = np.array(iris.data)
     y = np.array(iris.target)
     y_oh = np.eye(3)[y]
-    X, y = shuffle(X, y_oh, random_state=10)
+    X, y = shuffle(X, y_oh, random_state=42)
     X_train = X[:100]
     X_val = X[100:]
     y_train = np.array(y[:100])
     y_val = np.array(y[100:])
-    # X_train = X[:10]
-    # X_val = X[10:]
-    # y_train = np.array(y[:10])
-    # y_val = np.array(y[10:])
+    # X_train = X[:1]
+    # X_val = X[1:]
+    # y_train = np.array(y[:1])
+    # y_val = np.array(y[1:])
 
 
 
@@ -52,9 +53,12 @@ def running_the_network(n):
     X_train = std_slc.transform(X_train)
     X_val = std_slc.transform(X_val)
 
+    print(X_train)
+    print(y_train)
 
 
-    epochs = 20
+
+    epochs = 50
     train_acc = []
     train_rec = []
     train_pre = []
@@ -78,7 +82,10 @@ def running_the_network(n):
         validation_acc.append(bp.iris_evaluation(X_val, y_val))
 
         #n.print_states()
-        loss = bp.train(X_train, y_train, learning_rate = 1)
+        for i in n.Axon_dict.values():
+            if math.isnan(i.get_weight()):
+                print("pause")
+        loss = bp.train(X_train, y_train, learning_rate = 0.001)
         epoch_losses.append(np.average(loss))
         losses = np.vstack([losses, loss]) if len(losses) else loss
         # train_acc.append(bp.iris_evaluation(X_train, y_train))