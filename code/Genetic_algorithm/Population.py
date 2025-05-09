backpropagation loops fixed. iris working without bugs

code/Genetic_algorithm/Population.py
@@ -39,7 +39,7 @@ class Population:
 
         for timestep in np.arange(0,500):
             print("Generation:", timestep)
-            generation = self.generation(indiv_fit)
+            generation = self.generation(indiv_fit, timestep)
             generations.append(generation)
             indiv_fit = self.selection(pd.concat([generation, indiv_fit]))
 
@@ -64,10 +64,10 @@ class Population:
         return pd.DataFrame(generation, columns=["individual", "fitness"])
 
     # take the morpho rules from the previous generation for the next one
-    def generation(self, individuals_prev_generation):
+    def generation(self, individuals_prev_generation, timestep):
         generation = []
         for counter in np.arange(0, self.population_size-self.survivors):
-            print("Individual:", counter,"/",self.population_size)
+
             individual = Genetic_algorithm.Individual.Individual(environment=self.environment)
 
             # take the morpho rules and give them to the new generation
@@ -81,6 +81,7 @@ class Population:
                 if rule in individual.c.Rules.keys():
                     individual.c.Rules[rule].mutate()
             individual.morphogenesis_individual()
+            print("Individual:", counter,"/",self.population_size, "\tGeneration", timestep, "neurons:", len(individual.c.Cells.keys()))
             individual.running_the_network()
 
             generation.append([individual, individual.fitness_scores])
@@ -206,4 +207,6 @@ class Population:
         n = Neural_network.Neuron_space.NeuronSpace(Visualization=False)
         n.import_network(generations[-1].iloc[1,0].c)
         nn_exe.running_the_network(individual=generations[-1].iloc[1,0], n=n, viz=True)
+        
+        generations[-1].iloc[-5,0].c.start_vis()
         '''

code/Morphogen_simulation_v2/Cell_v2.py
@@ -18,6 +18,9 @@ class Cell():
         self.morphogens = {}
         self.output = output
         self.input = input
+        self.replication_counter = 0
+        self.integrated = False
+        self.integrated_checking = False
         # self.replicate_vector = Coordinates.Coordinate(0, 0, -1)
 
     def new_morphogens(self, new_morphogen_name):
@@ -42,13 +45,26 @@ class Cell():
             cell = self.cell_space.Cells[cell_name]
             if cell.name != self.name:
                 distance = max(1, Morphogen_simulation_v2.Coordinates.distance_finder(self.coordinate, cell.coordinate))
-                calculated = morphogen.amount/np.log(distance) # morphogen with distance falloff
+                calculated = (morphogen.amount/np.log(distance))/2 # morphogen with distance falloff
                 concentration += calculated
         return concentration
 
     def develop(self):
         for rule in self.cell_space.Rules.values():
             rule.rule(self)
+
+    def check_integration(self):
+        self.integrated_checking = True
+        if self.input == True:
+            self.integrated = True
+            return True
+        else:
+            for cell in self.parents.values():
+                if not cell.integrated_checking:
+                    if cell.check_integration():
+                        self.integrated = True
+            return self.integrated
+
 
 
     # def step(self):